17db9ec58ffa0c64d815884ea957e4ea4256a523
Prajna1999 <gituprajna20@gmail.com>
2023-03-31 16:16:42 +0530

    sidebar refining

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,33 @@
 import { Layout, Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
-
+import {Sidebar, Navbar} from '../components'
 function Home() {
   return (
-    <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
+    <>
+     
+    <Navbar />
+    <div className="flex flex-row justify-center items-center w-screen h-screen gap-24  bg-gray-100">
+      
+      {/* <section className="flex flex-col gap-6 bg-red-500">
         <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
         <Text className="text-zinc-600">
           In this example, a simple chat bot is implemented using Next.js, API
           Routes, and OpenAI API.
         </Text>
-      </section>
-
-      <section className="flex flex-col gap-3">
+      </section> */}
+        <Sidebar />
+        
+      <section className="flex w-8/12  flex-col gap-3">
         <Text variant="h2">AI Chat Bot:</Text>
-        <div className="lg:w-2/3">
+        <div className="container h-9/12 mx-auto">
           <Chat />
         </div>
       </section>
-    </Page>
+    </div>
+    </>
   )
 }
 
-Home.Layout = Layout
+// Home.Layout = Layout
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [require('@vercel/examples-ui/tailwind')],
+  // presets: [require('@vercel/examples-ui/tailwind')],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
